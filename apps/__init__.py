4558d483e6b8d8a032f54742f620e6b238067bc6
App Generator <51070104+app-generator@users.noreply.github.com>
2023-01-03 08:10:05 +0200

    DB management - Silent fallback to SQLite

--- apps/__init__.py
+++ apps/__init__.py
@@ -3,6 +3,8 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
+import os
+
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
@@ -28,11 +30,22 @@ def configure_database(app):
 
     @app.before_first_request
     def initialize_database():
-        db.create_all()
+        try:
+            db.create_all()
+        except Exception as e:
+
+            print('> Error: DBMS Exception: ' + str(e) )
+
+            # fallback to SQLite
+            basedir = os.path.abspath(os.path.dirname(__file__))
+            app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
+
+            print('> Fallback to SQLite ')
+            db.create_all()
 
     @app.teardown_request
     def shutdown_session(exception=None):
-        db.session.remove()
+        db.session.remove() 
 
 
 def create_app(config):
